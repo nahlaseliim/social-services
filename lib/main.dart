e6cb9a227fc29f6ac0782301258c3e47e709ca67
powar <nahlaaselim@gmail.com>
2025-03-19 16:12:44 +0200

    gg

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
+  // h
 }
 
 class MyApp extends StatelessWidget {
